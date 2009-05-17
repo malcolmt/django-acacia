77ff245411053bfc9c253b34a4b8caedb955c31a
Malcolm Tredinnick <malcolm@pointy-stick.com>
2009-05-17 12:07:17 +1000

    Changed runtests.py to permit running individual tests.

--- testing/runtests.py
+++ testing/runtests.py
@@ -10,20 +10,28 @@ importable as "treebeard" (that is, assumes it is on the Python path somewhere).
 import os
 import sys
 
-import test_settings
 from django.conf import settings
 from django.core import management
+
+import test_settings
 
 def main(argv=None):
     """
     Does the equivalent of "django-admin.py test acacia" after doing some basic
-    configuration.
+    configuration. Caller can also pass in the names of particular TestCase or
+    test method to run, as per the normal unittest calling style. In that case,
+    those tests are run, instead of the full suite.
     """
     if argv is None:
         argv = sys.argv
+    if len(argv) == 1:
+        # No particular tests requested. Run the whole suite.
+        args = [argv[0], "test", "acacia"]
+    else:
+        args = [argv[0], "test"] + argv[1:]
 
     # Ensure the local version of "acacia" is directly importable
-    pkg_dir = os.path.join(os.path.dirname(__file__), "..")
+    pkg_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     sys.path.insert(0, pkg_dir)
 
     options = {}
@@ -32,7 +40,7 @@ def main(argv=None):
             options[name] = getattr(test_settings, name)
     settings.configure(**options)
 
-    controller = management.ManagementUtility([argv[0], "test", "acacia"])
+    controller = management.ManagementUtility(args)
     controller.execute()
 
 
